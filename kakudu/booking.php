584d57cbb7c8c2079ca18b868a71e158d82edf12
Shravan-R <vibhavr234@gmail.com>
2025-01-09 21:34:52 +0530

    Update booking.php

--- kakudu/booking.php
+++ kakudu/booking.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $address_drop = $conn->real_escape_string($_POST['address_drop']);
     $size_of_product = $conn->real_escape_string($_POST['size_of_product']);
 
-    $sql = "INSERT INTO booking (date, address_pickup, address_drop, size_of_product) VALUES ('$date', '$address_pickup', '$address_drop', '$size_of_product')";
+    $sql = "INSERT INTO  booking (date, address_pickup, address_drop, size_of_product) VALUES ('$date', '$address_pickup', '$address_drop', '$size_of_product')";
 
     if ($conn->query($sql) === TRUE) {
         echo "<script>alert('Booking successfully Choose the available vehicle')</script>";
